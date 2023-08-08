4864bf277c0ca225f0ae99db9c9b66c105f7bc19
adanngrha <contact.ardana@gmail.com>
2023-08-08 23:55:27 +0700

    changed homepage into function component, add network-data.js

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,63 +4,37 @@ import NoteList from "../components/NoteList.jsx";
 import SearchBar from "../components/SearchBar.jsx";
 import {Link, useSearchParams} from "react-router-dom";
 import { MdNoteAdd } from "react-icons/md";
-import PropTypes from "prop-types";
 
-class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            notes: getActiveNotes(),
-            keyword: props.defaultKeyword || '',
-        }
-
-        this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
-    }
-
-    onKeywordChangeHandler(keyword) {
-        this.setState(() => {
-            return {
-                keyword,
-            }
-        });
-
-        this.props.keywordChange(keyword);
-    }
-
-    render() {
-        const notes = this.state.notes.filter((note) => {
-            return note.title.toLowerCase().includes(
-                this.state.keyword.toLowerCase()
-            );
-        });
-
-        return (
-            <section className="homepage">
-                <h2>Catatan Aktif</h2>
-                <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler}/>
-                <NoteList notes={notes}/>
-                <div className="homepage__action">
-                    <Link to="notes/new" className="action"><MdNoteAdd /></Link>
-                </div>
-            </section>
-        );
-    }
-}
-
-const HomePageWrapper = () => {
+const HomePage = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const keyword = searchParams.get('keyword');
-    function changeSearchParams(keyword) {
+    const [notes, setNotes] = React.useState([]);
+    const [keyword, setKeyword] = React.useState(() => {
+        return searchParams.get('keyword') || ''
+    });
+
+    React.useEffect(() => {
+        setNotes(getActiveNotes());
+    }, []);
+
+    const onKeywordChangeHandler = keyword => {
+        setKeyword(keyword);
         setSearchParams({ keyword });
     }
 
-    return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+    const filteredNotes = notes.filter(note => {
+        return note.title.toLowerCase().includes(keyword.toLowerCase());
+    });
+
+    return (
+        <section className="homepage">
+            <h2>Catatan Aktif</h2>
+            <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler}/>
+            <NoteList notes={filteredNotes}/>
+            <div className="homepage__action">
+                <Link to="notes/new" className="action"><MdNoteAdd /></Link>
+            </div>
+        </section>
+    );
 }
 
-HomePage.propTypes = {
-    defaultKeyword: PropTypes.string,
-    keywordChange: PropTypes.func.isRequired,
-}
-
-export default HomePageWrapper;
+export default HomePage;
